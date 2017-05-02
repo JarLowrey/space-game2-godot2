c4c8d0d76cadbd8ad8e786648084a2ade4ebbc28
James Lowrey <James Lowrey>
2017-05-02 17:27:27 -0400

    added a global scene

--- scenes/Gun/Gun.gd
+++ scenes/Gun/Gun.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var bullet = preload("res://scenes/Gun/Bullet.tscn")
+
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
---
+++ scripts/global.gd
@@ -0,0 +1,9 @@
+extends Node
+
+#singleton in godot:
+#http://docs.godotengine.org/en/2.1/learning/step_by_step/singletons_autoload.html
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
