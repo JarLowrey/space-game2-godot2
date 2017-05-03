4ea56b4ed1fbe480d0eeb8193d980285c7bb1620
james <jtronlabs@gmail.com>
2017-05-03 16:52:05 -0400

    working on gun stuff

--- src/scenes/Gun/Bullets/Bullet.gd
+++ src/scenes/Gun/Bullets/Bullet.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
-export var kill_out_of_bounds = false
-export var lifespan = 0 setget set_lifespan, get_lifespan
 var rigid_body = null
+var collision_polygon = null
+var sprite = null
 var gun_shot_from = null
+
 var _timer = null
+var kill_out_of_bounds = false
+var lifespan = 0 setget set_lifespan, get_lifespan
 
 func setup(shooting_gun, json):
 	gun_shot_from = shooting_gun
-	rigid_body = get_node(json.rigid_body_node_path).instance()
-	add_child(rigid_body)
-	return 0
+	collision_polygon = load(json.collision_polygon_scene).instance()
+	rigid_body = load(json.rigid_body_scene).instance()
+	
+	print(sprite)
+	sprite.add_child(rigid_body)
+	rigid_body.add_child(collision_polygon)
 
 func set_lifespan(span):
 	_timer.set_wait_time(span)
@@ -25,6 +31,7 @@ func _die():
 	return 
 
 func _ready():
+	sprite = get_node("Sprite")
 	_timer = Timer.new()
 	_timer.connect("timeout",self,"_die") 
 	pass
--- src/scenes/Gun/Gun.gd
+++ src/scenes/Gun/Gun.gd
@@ -4,14 +4,15 @@ var gun_texture = null
 var shots = []
 var gun_sprite = null
 
-var default_json = {
+var test_json = {
 	"gun_texture": "res://assets/imgs/gear/guns/gun01.png",
 	"shots": [
 		{
-			"bullet_node_path": "Bullets/Bullet",
+			"bullet_scene": "res://src/scenes/Gun/Bullets/Bullet.tscn",
 			"fire_from": { "x": 50, "y": 50 },
 			"params": {
-					"rigid_body_node_path": "res://assets/scenes"
+					"rigid_body_scene": "res://assets/godot/scenes/bodies/default_bullet.tscn",
+					"collision_polygon_scene": "res://assets/godot/scenes/polygon2Ds/enemy1.xml"
 				}
 		}
 	]
@@ -19,6 +20,8 @@ var default_json = {
 
 func _ready():
 	gun_sprite = get_node("GunSprite")
+	setup(test_json)
+	fire()
 	pass
 
 func setup(json):
@@ -30,11 +33,10 @@ func fire():
 		_fire_bullet(bullet_info)
 	
 func _fire_bullet(bullet_info):
-	print(bullet_info)
-	var bullet = get_node(bullet_info.bullet_node_path).instance()
+	var bullet = load(bullet_info.bullet_scene).instance()
 	
 	var percent_pos = Vector2(bullet_info.fire_from.x, bullet_info.fire_from.y) / 100.0
 	var pos_on_gun_sprite =  percent_pos * get_global_scale() + get_global_pos()
-	bullet.set_position(pos_on_gun_sprite)
+	bullet.set_pos(pos_on_gun_sprite)
 	
 	bullet.setup(self, bullet_info.params)
